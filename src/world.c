32838e4781707241abffb4941783aa6ae305df6e
Louis Peyrondet <louis.peyrondet@hotmail.fr>
2024-02-19 13:19:07 +0100

    Infinite world

---
+++ src/world.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include <world.h>
+
+world * world_init(){
+    world * w = malloc(sizeof(*w));
+    assert(w);
+    w->center_chunk = (point){.x = 0, .z = 0};
+    printf("world init center at %d,%d\n", w->center_chunk.x, w->center_chunk.z);
+    int index = 0;
+    for (int z = -RENDER_DISTANCE/2 ; z < RENDER_DISTANCE/2 ; z++){
+        for (int x = -RENDER_DISTANCE/2;  x < RENDER_DISTANCE/2; x++){
+            w->loaded_chunks[index] = chunk_init(x, z);
+            index++;
+        }
+    }
+
+    w->cached_chunks_count = 0;
+    w->cached_chunks = NULL;
+    
+    return w;
+}
+
+
+bool chunk_in_cache(world * w, int x , int z){
+    for (size_t i = 0; i < w->cached_chunks_count; i++){
+        if (w->cached_chunks[i]->x == x && w->cached_chunks[i]->z == z){
+            return true;
+        }
+    }
+    return false;
+}
+
+void world_append_chunk_cache(world * w, chunk * chunk){
+    w->cached_chunks_count++;
+    w->cached_chunks = realloc(w->cached_chunks, sizeof(*w->cached_chunks) * w->cached_chunks_count);
+    assert(w->cached_chunks);
+    w->cached_chunks[w->cached_chunks_count - 1] = chunk;
+}
+
+chunk * world_get_chunk_cache(world * w, int x , int z){
+    for (size_t i = 0; i < w->cached_chunks_count; i++){
+        if (w->cached_chunks[i]->x == x && w->cached_chunks[i]->z == z){
+            return w->cached_chunks[i];
+        }
+    }
+    fprintf(stderr, "Tried to get a chunk from the cache that wasn't inside !");
+    return NULL;
+}
+
+bool world_update_position(world * w, float x, float z){
+    point new_center = {.x = ((int)x) /16, .z = ((int)z) / 16};
+
+    if (new_center.x != w->center_chunk.x || new_center.z != w->center_chunk.z){
+        printf("new center chunk %d,%d (previous %d,%d)\n", new_center.x, new_center.z, w->center_chunk.x, w->center_chunk.z);
+        w->center_chunk = new_center;
+        int index = 0;
+        chunk * chunk;
+        for (int z = -RENDER_DISTANCE/2 ; z < RENDER_DISTANCE/2 ; z++){
+            for (int x = -RENDER_DISTANCE/2;  x < RENDER_DISTANCE/2; x++){
+                chunk = w->loaded_chunks[index];
+                // Put old chunk in the cache
+                if (!chunk_in_cache(w, chunk->x, chunk->z)){
+                    world_append_chunk_cache(w, chunk);
+                }
+
+                // Get new chunk from cache or generate it
+                if (chunk_in_cache(w, x + w->center_chunk.x, z + w->center_chunk.z)){
+                    w->loaded_chunks[index] = world_get_chunk_cache(w, x + w->center_chunk.x, z + w->center_chunk.z);
+                    assert(w->loaded_chunks[index]);
+                }else{
+                    w->loaded_chunks[index] = chunk_init(x + w->center_chunk.x, z + w->center_chunk.z);
+                }
+                index++;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+
+void world_load_chunk(world * w, int x, int z);
+void world_unload_chunk(world * w, int x, int z);
+
+void world_cleanup(world * w){
+    for (size_t i = 0; i < TOTAL_CHUNKS; i++){
+        chunk_cleanup(w->loaded_chunks[i]);
+    }
+    for (size_t i = 0; i < w->cached_chunks_count; i++){
+        chunk_cleanup(w->cached_chunks[i]);
+    }
+    free(w->cached_chunks);
+
+    free(w);
+}
---
+++ src/world.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include <chunk.h>
+
+#define RENDER_DISTANCE 8
+#define TOTAL_CHUNKS (RENDER_DISTANCE*RENDER_DISTANCE)
+
+typedef struct point {
+    int x;
+    int z;
+} point;
+
+typedef struct world {
+    // Needs a set somewhere to quickly check if a chunk is cached (retrieve it) or have been generated (load it) or needs to be generated (gen it)
+
+    // Make this a dynamic array 
+    chunk ** cached_chunks;
+    unsigned int cached_chunks_count;
+    chunk * loaded_chunks[TOTAL_CHUNKS];
+    point center_chunk;
+
+} world;
+
+
+world * world_init();
+
+bool world_update_position(world * w, float x, float z);
+void world_load_chunk(world * w, int x, int z);
+void world_unload_chunk(world * w, int x, int z);
+
+void world_cleanup(world * w);
