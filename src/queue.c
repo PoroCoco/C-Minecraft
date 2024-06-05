fb1270e9ab19168a9c669550ebb4300e87543be9
Louis Peyrondet <louis.peyrondet@hotmail.fr>
2024-06-05 17:43:15 +0900

    Added new queue using producer consumer pattern

--- src/queue.c
+++ src/queue.c
@@ -10,6 +10,8 @@ queue * queue_init(uint64_t size){
     q->back = 0;
     q->count = 0;
     pthread_mutex_init(&q->mutex, NULL);
+    pthread_cond_init(&q->emptied, NULL);
+    pthread_cond_init(&q->filled, NULL);
     return q;
 }
 
@@ -21,29 +23,27 @@ bool _queue_is_empty_lock_free(queue const* q){
     return ((q->back == q->front) && (q->count == 0));
 }
 
-void queue_enqueue(queue* q, void* value){
+void queue_produce(queue* q, void* value){
     pthread_mutex_lock(&q->mutex);
-    while(_queue_is_full_lock_free(q)){
-        // fprintf(stderr, "Tried to enqueue into a full queue !\n");
-        pthread_mutex_unlock(&q->mutex);
-        pthread_mutex_lock(&q->mutex);
+    while (_queue_is_full_lock_free(q)){
+        pthread_cond_wait(&q->emptied, &q->mutex);
     }
     q->container[q->back++] = value;
     q->back = q->back%q->size;
     q->count++;
+    pthread_cond_signal(&q->filled);
     pthread_mutex_unlock(&q->mutex);
 }
 
-void *queue_dequeue(queue* q){
+void *queue_consume(queue* q){
     pthread_mutex_lock(&q->mutex);
-    if (_queue_is_empty_lock_free(q)){
-        fprintf(stderr, "Tried to dequeue an empty queue !\n");
-        pthread_mutex_unlock(&q->mutex);
-        return NULL;
+    while (_queue_is_empty_lock_free(q)){
+        pthread_cond_wait(&q->filled, &q->mutex);
     }
     void * element = q->container[q->front++];
     q->front = q->front%q->size;
     q->count--;
+    pthread_cond_signal(&q->emptied);
     pthread_mutex_unlock(&q->mutex);
     return element;
 }
@@ -65,5 +65,7 @@ bool queue_is_empty(queue * q){
 void queue_cleanup(queue* q){
     free(q->container);
     pthread_mutex_destroy(&q->mutex);
+    pthread_cond_destroy(&q->filled);
+    pthread_cond_destroy(&q->emptied);
     free(q);
 }
--- src/queue.h
+++ src/queue.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+
+// Yes it's called a queue but if you try multiple concurrent enqueue on a full queue, then order is not guaranteed for them (surely I'll remember this)
 typedef struct queue {
     void ** container;
     uint64_t size;
@@ -11,12 +13,14 @@ typedef struct queue {
     uint64_t count;
     uint64_t back;
     pthread_mutex_t mutex;
+    pthread_cond_t emptied;
+    pthread_cond_t filled;
 } queue;
 
 
 queue * queue_init(uint64_t intial_size);
-void queue_enqueue(queue* q, void* value);
-void *queue_dequeue(queue* q);
+void queue_produce(queue* q, void* value);
+void *queue_consume(queue* q);
 bool queue_is_empty(queue* q);
 bool queue_is_full(queue* q);
 void queue_cleanup(queue* q);
